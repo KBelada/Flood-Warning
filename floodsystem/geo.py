f392770e622e49c8d080ce669ddb77430287f6d8
Tommy Ogilvie <tido2@cam.ac.uk>
2022-02-08 20:11:16 +0000

    task 1C initial code

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,19 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+
+def stations_within_radius(stations, centre, r):
+    #Function that returns the names of the monitoring stations within a given radius of a coordinate
+
+    within_radius = []
+
+    for station in stations:
+        if haversine(station.coord, centre) < r:
+            within_radius.append(station.name)
+    
+    within_radius = sorted(within_radius)
+
+    print(within_radius)
+
