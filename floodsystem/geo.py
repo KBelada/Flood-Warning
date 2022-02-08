9d186a381a76438d639ad73e7d877d7a5c03c374
Ksenija Belada <belada.ksenija@gmail.com>
2022-02-08 21:10:31 +0000

    added distance

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,16 @@ geographical data.
 
 """
 
+from ensurepip import version
+from os import stat
 from .utils import sorted_by_key  # noqa
+from floodsystem.stationdata import build_station_list
+from haversine import haversine, Unit
+
+def stations_by_distance(stations, p):
+    """Make a list of stations and their distanced from a fixed point p, then sort it in ascending order"""
+    distance_list=[] # empty 
+    for station in stations:
+        distance = haversine(station.coord, p)
+        distance_list.append((station.name, station.town, distance))
+    return sorted_by_key(distance_list, 2)
