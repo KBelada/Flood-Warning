c50401f7c8346fef4abc768db3b0577550a28b1a
Tommy Ogilvie <tido2@cam.ac.uk>
2022-02-09 22:43:54 +0000

    Added test for 1F

--- test_station.py
+++ test_station.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
 
 
 def test_create_monitoring_station():
@@ -25,3 +25,37 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_inconsistent_data():
+
+    # Create stations with inconsistent typical range data
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    b_id = "test-a-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (2.3, -3.4445)
+    river = "River X"
+    town = "My Town"
+    b = MonitoringStation(b_id, m_id, label, coord, trange, river, town)
+
+    a_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = None
+    river = "River X"
+    town = "My Town"
+    a = MonitoringStation(a_id, m_id, label, coord, trange, river, town)
+
+    c = [s, b, a]
+
+    assert inconsistent_typical_range_stations(c) == [a, b]
