45e1564fb06cf7bd23baa242e25b35e525a3f568
Tommy Ogilvie <tido2@cam.ac.uk>
2022-02-09 22:58:03 +0000

    possibly fixed test for 1F

--- test_station.py
+++ test_station.py
@@ -38,24 +38,24 @@ def test_inconsistent_data():
     town = "My Town"
     s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
 
-    b_id = "test-a-id"
-    m_id = "test-m-id"
-    label = "some station"
-    coord = (-2.0, 4.0)
+    b_id = "test-b-id"
+    m_id = "test-mb-id"
+    label = "that station"
+    coord = (-2.0, 5.0)
     trange = (2.3, -3.4445)
-    river = "River X"
-    town = "My Town"
+    river = "River B"
+    town = "Your Town"
     b = MonitoringStation(b_id, m_id, label, coord, trange, river, town)
 
-    a_id = "test-s-id"
-    m_id = "test-m-id"
-    label = "some station"
-    coord = (-2.0, 4.0)
+    a_id = "test-a-id"
+    m_id = "test-ma-id"
+    label = "a station"
+    coord = (-2.0, 6.0)
     trange = None
-    river = "River X"
-    town = "My Town"
+    river = "River A"
+    town = "Their Town"
     a = MonitoringStation(a_id, m_id, label, coord, trange, river, town)
 
     c = [s, b, a]
 
-    assert inconsistent_typical_range_stations(c) == [a, b]
+    assert inconsistent_typical_range_stations(c) == ["a station", "that station"]
