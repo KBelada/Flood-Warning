8eaed5a060f6fadbf243e1d32814d10cfa129013
Ksenija Belada <belada.ksenija@gmail.com>
2022-03-01 19:12:10 +0000

    2E finished

---
+++ Task2E.py
@@ -0,0 +1,26 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.plot import plot_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+import datetime
+
+def run():
+    """Requirements for 2E"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    # Get top 5 stations
+    highest_five = stations_highest_rel_level(stations, 5)
+
+    for i in highest_five: # iterate thorugh them
+        dates, levels = fetch_measure_levels(i.measure_id, datetime.timedelta(days = 10)) # Get dates and levels for the past 10 days
+        plot_water_levels(i, dates, levels) # plot the corresponding values
+
+
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
